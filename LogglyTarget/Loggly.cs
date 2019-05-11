61cafff00fad88b3cebc49e0d228dcd50c8e3173
Tiernan OToole <tot@ersl.ie>
2019-05-11 23:07:32 +0100

    MAJOR UPDATE! .NET 4.7 now targeted. NLog 4.6.3 support. Using Flurl for writing to Loggly. Support for multi writes in one call (buffered)

--- LogglyTarget/Loggly.cs
+++ LogglyTarget/Loggly.cs
@@ -1,105 +1,89 @@
-﻿using System.Collections.Generic;
-using System.Text;
+﻿using System;
+using System.Collections.Generic;
 using NLog.Targets;
 using NLog;
-using System.Net;
-using System.IO;
+using System.Net.Http;
+using Flurl.Http;
+using Newtonsoft.Json;
 
 namespace LogglyTarget
 {
-    [Target("Loggly")]
-    public class Loggly : TargetWithLayout
-    {
+	[Target("Loggly")]
+	public class Loggly : TargetWithLayout
+	{
+		readonly List<string> mBuffer;
 
-        List<string> buffer;
-        public Loggly()
-        {
-            this.URL = "NotSet";
-            bufferNumber = 1;
-            shouldBuffer = false;
-            buffer = new List<string>();
-        }
+		public string Url { get; set; }
+		public int BufferNumber { get; set; }
+		public bool ShouldBuffer { get; set; }
 
+		public Loggly()
+		{
+			Url = "NotSet";
+			BufferNumber = 1;
+			ShouldBuffer = false;
+			mBuffer = new List<string>();
+		}
 
-        public string URL { get; set; }
-        public int bufferNumber { get; set; }
-        public bool shouldBuffer { get; set; }
-
-        protected override void Write(LogEventInfo logEvent)
-        {
-            //if the target has not been set correctly, dont log...
-            if(URL != "NotSet")
-            {
-                string logMessage = this.Layout.Render(logEvent);
-                sendMessageToLoggly(logMessage);
-            }
-        }
-        protected override void Dispose(bool disposing)
-        {
-            if (buffer.Count > 0)
-            {
-                buffer.ForEach(b => WriteToURL(b));
-                buffer.Clear();
-            }
-            base.Dispose(disposing);
-        }
-
+		protected override void Write(LogEventInfo logEvent)
+		{
+			//if the target has not been set correctly, don't log...
+			if (Url != "NotSet")
+			{
+				var logMessage = Layout.Render(logEvent);
+				SendMessageToLoggly(logMessage);
+			}
+		}
+		protected override void Dispose(bool disposing)
+		{
+			if (mBuffer.Count > 0)
+			{
+				mBuffer.ForEach(b => WriteToUrl(b));
+				mBuffer.Clear();
+			}
+			base.Dispose(disposing);
+		}
 
 
-        private bool WriteToURL(string message)
-        {
-            HttpWebRequest req = null;
-            req = (HttpWebRequest)WebRequest.Create(URL);
-            req.Method = "POST";
-            req.ContentType = "application/x-www-from-urlencoded";
-            req.ContentLength = message.Length;
-            using (Stream write = req.GetRequestStream())
-            {
-                UTF8Encoding enc = new UTF8Encoding();
-                byte[] data = enc.GetBytes(message);
-                write.Write(data, 0, data.Length);
-            }
-            using (HttpWebResponse resp = (HttpWebResponse)req.GetResponse())
-            {
-                if (resp.StatusCode != HttpStatusCode.OK)
-                {
-                    return false;
-                }
-                else
-                {
-                    using (Stream respStream = resp.GetResponseStream())
-                    {
-                        using (StreamReader reader = new StreamReader(respStream))
-                        {
-                            string result = reader.ReadToEnd();
-                            //todo... do something with this...
-                            return true;
-                        }
-                    }
-                }
-            }
-        }
+		private bool WriteToUrl(string message)
+		{
+			var result = Url.PostAsync(new StringContent(message)).ReceiveJson<LogglyResult>().GetAwaiter().GetResult();
 
-        private void sendMessageToLoggly(string message)
-        {
-            //is buffering enabled?
-            if (shouldBuffer)
-            {
-                //write message to buffer
-                buffer.Add(message);
-                //if buffer is full, send to loggly
-                if (buffer.Count == bufferNumber)
-                {
-                    buffer.ForEach(b => WriteToURL(b));     //todo: something to check that we get a true back from the writer... 
-                    //todo: only remove items that have been sent, so older ones still eventually get logged...
-                    //clear buffer
-                    buffer.Clear();
-                }
-            }
-            else
-            {
-                WriteToURL(message);
-            }
-        }
-    }
+			return result.Response == "ok";
+		}
+
+		
+
+		private void SendMessageToLoggly(string message)
+		{
+			//is buffering enabled?
+			if (ShouldBuffer)
+			{
+				//write message to buffer
+				mBuffer.Add(message);
+				//if buffer is full, send to loggly
+				if (mBuffer.Count >= BufferNumber)
+				{
+					var items = string.Join(Environment.NewLine, mBuffer);
+
+					if (WriteToUrl(items))
+					{
+						mBuffer.Clear();
+					}
+				}
+			}
+			else
+			{
+				WriteToUrl(message);
+			}
+		}
+	}
+
+
+	public class LogglyResult
+	{
+		[JsonProperty(PropertyName = "Response" )]
+		public string Response { get; set; }
+	}
+
 }
--- LogglyTarget/Properties/AssemblyInfo.cs
+++ LogglyTarget/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("LSN")]
 [assembly: AssemblyProduct("LogglyTarget")]
-[assembly: AssemblyCopyright("Copyright © LSN 2015")]
+[assembly: AssemblyCopyright("Copyright © LSN 2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.2.*")]
-[assembly: AssemblyFileVersion("0.2.0.0")]
+[assembly: AssemblyVersion("0.3.*")]
+[assembly: AssemblyFileVersion("0.3.0.0")]
